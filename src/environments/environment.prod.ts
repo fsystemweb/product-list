92c8ac836355d3ae0dbae5d0e233cc83ee1c4d7f
Facundo Ezequiel Diaz Cappella <43161950+fsystemweb@users.noreply.github.com>
2025-03-17 18:20:52 +0100

    Vercel deploy (#9)
    * fix tsconfig app
    
    * update types
    
    * add pre-build
    
    * fix install
    
    * add empty types
    
    * remove file replace
    
    * remove errors
    
    * restore settings
    
    * update package json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  graphqlServerUrl: process.env['GRAPHQL_SERVER_URL'],
-  graphqlServerId: process.env['GRAPHQL_SERVER_ID'],
-  graphqlServerToken: process.env['GRAPHQL_SERVER_TOKEN'],
+  graphqlServerUrl: '{{GRAPHQL_SERVER_URL}}',
+  graphqlServerId: '{{GRAPHQL_SERVER_ID}}',
+  graphqlServerToken: '{{GRAPHQL_SERVER_TOKEN}}',
 };
