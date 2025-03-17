3f58800ade64165fe3ee16454510e2d41ff356e1
fsystemweb <facu.cappella@gmail.com>
2025-03-17 19:29:48 +0100

    hotfix: spinner momentum

--- src/app/state/category-state.service.ts
+++ src/app/state/category-state.service.ts
@@ -1,9 +1,10 @@
 import { DestroyRef, inject, Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, finalize, Observable } from 'rxjs';
 import { Category } from '../models/category';
 import { CategoryService } from '../services/category.service';
 import { ApolloArray } from '../models/arrays-apollo';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { LoadingService } from '../services/loading.service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,6 +12,7 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 export class CategoryStateService {
   private categoryService: CategoryService = inject(CategoryService);
   private destroyRef = inject(DestroyRef);
+  private loadingService = inject(LoadingService);
 
   private categoriesSubject = new BehaviorSubject<ApolloArray<Category> | null>(null);
   categories$: Observable<ApolloArray<Category> | null> = this.categoriesSubject.asObservable();
@@ -20,9 +22,13 @@ export class CategoryStateService {
   }
 
   loadCategories(): void {
+    this.loadingService.show();
     this.categoryService
       .getCategories()
-      .pipe(takeUntilDestroyed(this.destroyRef))
+      .pipe(
+        takeUntilDestroyed(this.destroyRef),
+        finalize(() => this.loadingService.hide())
+      )
       .subscribe(result => {
         this.categoriesSubject.next(result.data);
       });
